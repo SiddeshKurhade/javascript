b78d0dd501229b32b715be5d72549b9a6c4f3a6e
Siddesh Kurhade <113359866+SiddeshKurhade@users.noreply.github.com>
2025-04-03 08:48:16 +0000

    new

---
+++ Javascript_/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";   //treat all JS code as newer version
+
+// alert("hello")   //we are using NodeJS , not browser
+
+let name = "siddesh";
+let age = 21;
+let isLoggedIn = true;
+
+//Primitive DATA TYPES
+
+//number ==> range -> 2^53 
+//bigint
+//string => ""
+//boolean  => true & false
+//null => standalone value
+//undefined => if value is not declare
+//symbol => find uniqueness in our project
+
+
+
+//Object
+console.log(typeof("sidd"));
+console.log(typeof(33));
+console.log(typeof(null));       //=> null is a object
+console.log(typeof(undefined));  //=> it is a datatype 
