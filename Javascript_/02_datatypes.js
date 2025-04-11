7ee46ce288ce8ab6e0f0c279c6c3e8ab627dcb73
Siddesh Kurhade <113359866+SiddeshKurhade@users.noreply.github.com>
2025-04-11 06:47:12 +0000

    comparison

--- Javascript_/02_datatypes.js
+++ Javascript_/02_datatypes.js
@@ -2,9 +2,9 @@
 
 // alert("hello")   //we are using NodeJS , not browser
 
-let name = "siddesh";
-let age = 21;
-let isLoggedIn = true;
+// let name = "siddesh";
+// let age = 21;
+// let isLoggedIn = true;
 
 //Primitive DATA TYPES
 
@@ -19,7 +19,48 @@ let isLoggedIn = true;
 
 
 //Object
-console.log(typeof("sidd"));
-console.log(typeof(33));
-console.log(typeof(null));       //=> null is a object
-console.log(typeof(undefined));  //=> it is a datatype 
+// console.log(typeof("sidd"));
+// console.log(typeof(33));
+// console.log(typeof(null));       //=> null is a object
+// console.log(typeof(undefined));  //=> it is a datatype 
+
+
+
+// DATATYPE SUMMERY
+//Primitive datatype-> total 7 types
+//all are call by value
+// String , Number , Boolean, null ,undefined ,Symbol(it is used for to make unique value),BigInt
+
+
+//symbol ex=>
+// const id = Symbol("20")
+// console.log(id)
+
+
+
+
+//Reference type(Non primitive) 
+//Arrays , Objects ,Functions
+//Basic ex->
+
+//array
+const fruits=["Apple","Mango","Orange"];
+
+
+//object
+let myObj ={ 
+    name:"Siddesh",
+    age:21,
+}
+
+//function
+const myFunction = function(){
+    console.log("Hello");
+}
+
+console.log(typeof myObj)         //returns object
+console.log(typeof fruits)        //returns object
+console.log(typeof myFunction)    //returns function
+
+
+ 
---
+++ Javascript_/04_comparison_datatype.js
@@ -0,0 +1,27 @@
+//basic comparison & it returns boolean value
+// console.log(2 > 1)
+// console.log(2 <= 1)
+
+// //but we compare with different datatype
+// console.log("2">1)       //it returns true
+// console.log("02">1)      //it returns true
+
+
+
+// console.log(null >= 0)       //true
+// console.log(null > 0)        //false
+// //an equality check == & comparison > , < ,>= ,<= work differently
+// //comparison convert null to a number , treating it as 0
+// // thats why null>=0 is true & null > 0 is false
+// console.log(null==0)        //false
+
+
+
+// console.log(undefined == 0)     //false
+// console.log(undefined >= 0)     //false
+
+// ===(strick check)  => it checks value as well as datatype also
+// console.log("2" === 2)  //here value is same but datatype is different therefore output is false
+// console.log(1 == 1)     //here datatype(int) & value is same therfore it returns true
+// console.log(2===4) //here datatype is same but value is different therefor it returns false
+// console.log("2"=="2")  //here datatype(string) & value is same therfore it returns true
